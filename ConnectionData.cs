f4a283ffb6271340e90b1444f41fceb8ff5ad66c
ChristianJaredSantiagoHernandez <Cristianjared574@gmail.com>
2026-07-03 23:12:01 -0600

    Conexion a base de datos (clase)

---
+++ ConnectionData.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto
+{
+    internal class ConnectionData
+    {
+        private readonly string cadena; 
+
+        public ConnectionData() 
+        { 
+            cadena = "Server=localhost; Database=puntodb; UserID=root; Password=; Port=3306; SslMode=None;"; 
+        }
+        public MySqlConnection getConection()
+        {
+            try
+            {
+                MySqlConnection conexion = new MySqlConnection(cadena);
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar a la base de datos: " + ex.Message);
+                return null;
+            }
+
+        }
+    }
+}
